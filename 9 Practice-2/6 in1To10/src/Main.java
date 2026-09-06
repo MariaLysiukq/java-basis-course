2c6702cb9611f9af0b62ab28fc38975a63ceb0a1
Maria Lysiuk <marialysiukq@gmail.com>
2026-09-06 12:29:51 +0300

    solve second block practice

---
+++ 9 Practice-2/6 in1To10/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public boolean in1To10(int n, boolean outsideMode) {
+        if (outsideMode)
+        {
+            return n <= 1 || n >= 10;
+        }
+        else
+        {
+            return n >= 1 && n <= 10;
+        }
+    }
+}
