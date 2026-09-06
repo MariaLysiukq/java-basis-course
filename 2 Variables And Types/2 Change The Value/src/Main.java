d43568567527f6e0ab6952ae5ae41c9507861ed5
Maria Lysiuk <marialysiukq@gmail.com>
2026-09-06 12:25:19 +0300

    solve second block java - variables

---
+++ 2 Variables And Types/2 Change The Value/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 10;
+        // Присвойте score нове значення
+        score = 25;
+        System.out.println(score);
+    }
+}
---
+++ 2 Variables And Types/5 Meaningful Name/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int studentCount = 12;
+        System.out.println(studentCount);
+    }
+}
