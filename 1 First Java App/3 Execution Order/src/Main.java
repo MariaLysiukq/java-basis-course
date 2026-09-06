fb0e151fbef67cd3111f8e3a197cb534b88a04f9
Maria Lysiuk <marialysiukq@gmail.com>
2026-09-06 12:24:49 +0300

    solve first block java app tasks

---
+++ 1 First Java App/3 Execution Order/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Program started");
+        System.out.println("Processing data");
+        System.out.println("Program finished");
+    }
+}
