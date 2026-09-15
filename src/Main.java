c836daeed2534ced2e0c72b02d36721a051ae82b
MariaLysiuk <9727290@stud.op.edu.ua>
2026-09-15 15:49:23 +0300

    Refactor class and variable in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         System.out.println("Part A: Player");
+
         Player player1 = new Player("Maria", 100, 50);
         Player player2 = new Player("Lancelot", 20, 10);
+
         System.out.println("Initial State:");
         System.out.println(player1);
         System.out.println(player2);
@@ -28,18 +28,21 @@ public class Main
         System.out.println("Lancelot attack success: " + lancelotAttack);
 
         System.out.println("\nPart B: WaterTank");
-        WaterTank tank1 = new WaterTank("Tank-A", 100, 20);
-        WaterTank tank2 = new WaterTank("Tank-B", 50, 50);
+
+        WaterTank tank1 = new WaterTank("Tank-Alpha", 100, 20);
+        WaterTank tank2 = new WaterTank("Tank-Beta", 50, 50);
+
         System.out.println("Initial State:");
         System.out.println(tank1);
         System.out.println(tank2);
 
-        System.out.println("\nOperations on Tank-A:");
+        System.out.println("\nOperations on Tank-Alpha:");
         boolean fillA1 = tank1.fill(90);
         boolean fillA2 = tank1.fill(30);
         boolean drainA1 = tank1.drain(60);
         boolean drainA2 = tank1.drain(10);
-        System.out.println("Operations on Tank-B:");
+
+        System.out.println("Operations on Tank-Beta:");
         boolean isBFullInit = tank2.isFull();
         boolean fillB1 = tank2.fill(10);
         boolean drainB1 = tank2.drain(20);
@@ -47,12 +50,15 @@ public class Main
 
         System.out.println("\nFinal State:");
         System.out.println(tank1);
-        System.out.println("Tank-A fill(90) success: " + fillA1);
-        System.out.println("Tank-A fill(30) success: " + fillA2);
-        System.out.println("Tank-A drain(60) success: " + drainA1);
+        System.out.println("Tank-Alpha fill(90) success: " + fillA1);
+        System.out.println("Tank-Alpha fill(30) success: " + fillA2);
+        System.out.println("Tank-Alpha drain(60) success: " + drainA1);
+        System.out.println("Tank-Alpha drain(10) success: " + drainA2);
+
         System.out.println(tank2);
-        System.out.println("Tank-B was full initially: " + isBFullInit);
-        System.out.println("Tank-B fill(10) success: " + fillB1);
-        System.out.println("Tank-B is full now: " + isBFullAfter);
+        System.out.println("Tank-Beta was full initially: " + isBFullInit);
+        System.out.println("Tank-Beta fill(10) success: " + fillB1);
+        System.out.println("Tank-Beta drain(20) success: " + drainB1);
+        System.out.println("Tank-Beta is full now: " + isBFullAfter);
     }
 }
